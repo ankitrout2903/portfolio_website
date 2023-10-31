2fb9ebe973f5d1c53f02907e5d2759fe019bd91b
ankitrout2903 <88599131+ankitrout2903@users.noreply.github.com>
2023-10-31 20:11:31 +0530

    code

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 export const data = [
   {
     id: 1,
-    img: './src/assets/pregchat.png',
+    img: "./src/assets/pregchat.png",
     title: "PregChat",
     desc: "PregChat is a chatbot designed to help expecting mothers access important information about their pregnancy in a simple, accessible and interactive way. Our chatbot is powered by Dialogflow, a Google Cloud-based natural language processing platform. With PregChat, mothers can easily ask about their symptoms and receive information on medical",
     link: "https://pregchat.vercel.app/",
@@ -9,19 +9,18 @@ export const data = [
   },
   {
     id: 2,
-    img: './src/assets/leetclone.png',
+    img: "./src/assets/leetclone.png",
     title: "Leetclone-AI",
-    desc: "",
+    desc: "This is a LeetCode clone project created with Next.js, TypeScript, Tailwind CSS, OpenAI API, Firebase, Firestore, and Authentication. It allows users to practice coding problems, translate code between different programming languages, and save their solutions for reference.",
     link: "",
     git: "https://github.com/ankitrout2903/leetcolone-AI",
   },
   {
     id: 3,
-    img: './src/assets/querell-ai.png',
+    img: "./src/assets/querell-ai.png",
     title: "Querell-AI",
     desc: "Querell AI 🐿️✅ Interviewer - a game-changing chat bot designed to elevate your technical interviews! Powered by OpenAI's formidable API, this innovative AI interviewer focuses on React and Node.js development. Seamlessly assess candidates' proficiency, coding skills, and problem-solving abilities with precision.",
     link: "https://querell-ai-ankitrout2903.vercel.app/",
     git: "https://github.com/ankitrout2903/QuerellAI",
-  }
-  
+  },
 ];
